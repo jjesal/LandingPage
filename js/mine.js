8bd8eb6b8969244bcacc45d6ffde5b79a194c5ac
Emmy Antonio <emmyseco@gmail.com>
2020-04-13 17:32:02 -0500

    avances de la landing page becas

--- js/mine.js
+++ js/mine.js
@@ -1,4 +1,9 @@
+const MAXBECAS = 1;
+const URL_WEBSERVICE = "https://localhost/smartknowledgesolution/php/api.php";
+const URL_WEBSERVICE_EDUKT = "https://eduktvirtual.com/landingpage/formlandingpage.php";
 var code; var number; var pais; var departamento = { codigo: '', text: '' }; var ciudad = { codigo: '', text: '' };
+var sizeNumber = null;
+
 function enviar() {
     let data = {
         nombre: $('#inputName').val(),
@@ -33,13 +38,150 @@ function enviar() {
         })
     })
 }
+/*
+valida que el usuario ya tiene una beca
+*/
+function validarbeca(){
+    let emailCustomer = $('#inputEmail').val();
+    let datos = {
+        m: 'obtenerbecas'
+    }
+    $.post(URL_WEBSERVICE, datos, function(data, textStatus, xhr) {
+        if(data.code == 200){
+            let index = null;
+            if(Object.keys(data.data).length >= MAXBECAS){
+                Swal.fire({
+                    title: 'Becas agotadas',
+                    text: "Lo sentimos las becas se encuentran agotadas",
+                    icon: 'error',
+                })
+                return false;
+            }
+            //buscar el usuario
+            index = data.data.findIndex((el)=>el.email == emailCustomer);
+            if(index != -1 || index != null){
+                Swal.fire({
+                    title: 'Becas consumida',
+                    text: "Lo sentimos pero usted cuenta con una beca en estos momentos",
+                    icon: 'warning',
+                })
+            }else{
+                $('#referidoModal').modal('show');
+            }
+        }else{
+            console.error(data.message);
+        }
+    },'json');
+}
+function guardarbeca(){
+    let datos = {
+        m: 'guardarbecas',
+        nombres: $('#inputName').val(),
+        apellidos: $('#inputLastName').val(),
+        email: $('#inputEmail').val(),
+        telefono: $('#inputphone1').val(),
+        pais: $('#select-pais').val(),
+        departamento: $('#select-departamento').val(),
+        ciudad: $('#select-ciudad').val(),
+        curso: $('#select-cursos option:selected').text(),
+        codigo_curso: $('#select-cursos option:selected').data('codigo'),
+        tipocurso: $('#select-cursos').val()
+    }
+    $.post(URL_WEBSERVICE, datos, function(data, textStatus, xhr) {
+        if(data.code == 200){
+            guardarReferido(data.data.id);
+        }else{
+            console.error(data.message);
+        }
+    },'json');
+}
+function guardarReferido(_idbeca){
+    let datos = {
+        m: 'guardarreferidobecas',
+        idbeca: _idbeca,
+        nombres: $('#nameref').val(),
+        apellidos: $('#aperef').val(),
+        email: $('#emailref').val(),
+        telefono: $('#inputphone1ref').val(),
+        pais: $('#select-paisref').val(),
+        departamento: $('#select-departamentoref').val(),
+        ciudad: $('#select-ciudadref').val(),
+        curso: $('#select-cursos option:selected').text(),
+        tipocurso: $('#select-cursos').val()
+    }
+    $.post(URL_WEBSERVICE, datos, function(data, textStatus, xhr) {
+        if(data.code == 200){
+            console.log("referido guardado");
+        }else{
+            console.error(data.message);
+        }
+    },'json');
+}
+/*
+Procesa la matricula del cliente automaticamente
+*/
+function procesarMatricula(){
+    let idempresa='LIMA0000001';
+    let datos = {};
 
-var sizeNumber = null;
+    guardarbeca(); //guardar info en db de sks al becario y el referido
+    switch($('#select-pais').val()){
+        case 'Argentina': idempresa = 'ARGENTINA01';
+        break;
+        case 'Bolivia': idempresa = 'BOLIVIA0001';
+        break;
+        case 'Chile': idempresa = 'CHILE000001';
+        break;
+        case 'Colombia': idempresa = 'COLOMBIA001';
+        break;
+        case 'Costa Rica': idempresa = 'COSTARICA01';
+        break;
+        case 'Ecuador': idempresa = 'ECUADOR0001';
+        break;
+        case 'Mexico': idempresa = 'MEXICO00001';
+        break;
+        case 'Peru': idempresa = 'LIMA0000001';
+        break;
+    }
+    datos = {
+        m: 'matricular_edutkvirtual',
+        nombres: $('#inputName').val(),
+        apellidos: $('#inputLastName').val(),
+        email: $('#inputEmail').val(),
+        telefono: $('#inputphone1').val(),
+        pais: $('#select-pais').val(),
+        departamento: $('#select-departamento').val(),
+        iddepartamento: departamento.iddepartamento,
+        ciudad: $('#select-ciudad').val(),
+        curso: $('#select-cursos option:selected').text(),
+        codigo_curso: $('#select-cursos option:selected').data('codigo'),
+        programa: $('#select-cursos option:selected').data('codigo'),
+        idempresa: idempresa,
+        tipocurso: $('#select-cursos').val()
+    }
+    $.post(URL_WEBSERVICE_EDUKT, datos, function(data, textStatus, xhr) {
+        if(data.code == 'ok'){
+            Swal.fire({
+                title: 'Enviado',
+                text: "En seguida nos pondremos en contacto contigo!",
+                icon: 'success',
+
+            }).then((result) => {
+                window.location.reload();
+            })
+        }else{
+            console.error(data.message);
+        }
+    },'json');
+}
+/*
+valida que el usuario ya tiene una beca
+*/
 function validate() {
     let code = $('#inputcode1').val();
     let number = $('#inputphone1').val();
     if (number!='') {
-        enviar();
+        validarbeca();
         return false;
     } else {
         alert("El número que ingresaste no es valido");
@@ -51,6 +193,8 @@ function validate() {
 
 $(document).ready(function () {
     $('#ciudad-cont').hide();
+    $('#select-cursos').select2();
+
     fetch('./json/countries.json', {
         method: 'GET',
     }).then(rsp => {
@@ -62,11 +206,27 @@ $(document).ready(function () {
         maped.unshift({ id: '0', code: "", text: "País...", short: "", lista: [] })
         // console.log(maped);
         let arrDepartamentos = new Array;
-        arrDepartamentos.unshift({ id: '0', text: 'Departamento...', codigo: '', selected: true });
+        arrDepartamentos.unshift({ id: '0', text: 'Departamento...', codigo: '',iddepartamento:'0', selected: true });
         $('#select-departamento').select2({ data: arrDepartamentos });
         // $('#select-departamento').select2({ data: maped[0].lista });
         $('#select-pais').select2({ data: maped });
     })
+    $('body').on('click','#on-matricular',function(){
+        let _continue = true;
+        //validar campos
+        $('#referidoModal').find('.data-required').css('boder','');
+        $('#referidoModal').find('.data-required').each(function(index, el) {
+            if($(this).val() == '' || $(this).val() == 0){
+                $(this).css('boder','2px solid red');
+                _continue = false;
+            }    
+        });
+        //verificar
+        if(_continue == true){
+            $('#referidoModal').modal('hide');
+            procesarMatricula();
+        }
+    });
     $('#select-pais').on("select2:select", (e) => {
         clearCodes();
         pais = e.params.data;
@@ -80,11 +240,11 @@ $(document).ready(function () {
                 // console.log('get: ', arrDepartamentos)
                 let arrTmp = arrDepartamentos.map(departamento => {
                     if (pais.short === 'PE') {
-                        return { id: departamento.nombre, codigo: departamento.codteldep, text: departamento.nombre }
+                        return { id: departamento.nombre, codigo: departamento.codteldep, text: departamento.nombre,iddepartamento:departamento.iddepartamento }
                     } else if (pais.short === 'MEX' || pais.short === 'CHL' || pais.short === 'PAN' || pais.short === 'ARG') {
-                        return { id: departamento.district, codigo: "", text: departamento.district }
+                        return { id: departamento.district, codigo: "", text: departamento.district,iddepartamento:0 }
                     } else {
-                        return { id: departamento.name, codigo: '', text: departamento.name }
+                        return { id: departamento.name, codigo: '', text: departamento.name,iddepartamento:0 }
                     }
                 });
                 arrDepartamentos = arrTmp;
